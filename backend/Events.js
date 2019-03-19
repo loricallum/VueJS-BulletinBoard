fc631e1e0a700a316d95f1dadefad41834848d32
LLewis <lorimlewis@gmail.com>
2019-03-19 22:16:07 +0000

    Add files via upload

---
+++ backend/Events.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'Antioxidants',
+    detail: 'Purple foods, Green foods',
+    date: ''
+  },
+  {
+    id: 2,
+    title: 'Superfoods',
+    detail: 'Green superfoods',
+    date: ''
+  },
+  {
+    id: 3,
+    title: 'Micro nutrients'
+  }
+];
